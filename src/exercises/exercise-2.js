c9be85932e6c8aa27802e3765c423fa2f13e0e3d
chioarenillas <arenillasr@gmail.com>
2025-10-07 21:08:33 +0200

    changes

--- src/exercises/exercise-2.js
+++ src/exercises/exercise-2.js
@@ -4,7 +4,7 @@ Muestra el siguiente mensaje dependiendo de si un número es par o impar:
 “Number 34 is even” ó “Number 13 is odd”
 Nota: even = par | odd = impar
 */
-/* 
+
 
   function evenOdd(number){
     if( number % 2 == 0){ 
@@ -20,4 +20,4 @@ evenOdd(13);
 
 
 
- */
+ 
